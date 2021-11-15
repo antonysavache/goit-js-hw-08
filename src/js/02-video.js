53b7631bcf47ad2c2eb192bb4fc563a79051554c
antonysavache <89783847+antonysavache@users.noreply.github.com>
2021-11-15 18:06:42 +0200

    oops

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash';
+let time = 0;
 
+let localShron = {key:time}
+
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onPlay = function(data) {
+    localShron.key = data
+    console.log(localShron.key)
+};
+
+player.on('timeupdate', onPlay);
+
+player.setCurrentTime(localShron.key)
